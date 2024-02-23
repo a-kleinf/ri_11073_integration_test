30c8e28c1eb40e985d0db0fb5cf30ab35ba8676a
Albrecht Kleinfeld <albrecht.kleinfeld@draeger.com>
2024-02-23 11:32:46 +0100

    Add sdc11073 test - reference_consumer

--- sdc11073_consumer.sh
+++ sdc11073_consumer.sh
@@ -9,9 +9,10 @@ export ref_ssl_passwd=dummypass
 echo "Starting SDCri provider"
 (cd ri && sleep 999999999 | mvn -Pprovider-tls -Pallow-snapshots exec:java) &
 sleep 30
-echo "Starting sdc11073 consumer"
-cd sdc11073_git && python3 -m unittest examples.ReferenceTest.test_reference.Test_Reference.test_client_connects
-test_exit_code=$?
+echo "Starting sdc11073 consumer reference_consumer"
+python3 -m examples.ReferenceTest.reference_consumer, ((test_exit_code = $?))
+echo "Starting sdc11073 consumer test_client_connects"
+cd sdc11073_git && python3 -m unittest examples.ReferenceTest.test_reference.Test_Reference.test_client_connects; ((test_exit_code = test_exit_code || $?))
 
 echo "Terminating SDCri provider"
 jobs && kill %1
