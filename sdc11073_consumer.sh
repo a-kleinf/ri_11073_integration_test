4b7f8fde90c805a9aa3fb6824d300360d68bd302
Albrecht Kleinfeld <albrecht.kleinfeld@draeger.com>
2024-06-28 15:07:22 +0200

    remove not needed test

--- sdc11073_consumer.sh
+++ sdc11073_consumer.sh
@@ -28,8 +28,6 @@ sleep 20
 cd sdc11073_git 
 echo "Starting sdc11073 consumer reference_client"
 python3 -m examples.ReferenceTest.reference_client; ((test_exit_code = $?))
-echo "Starting sdc11073 consumer test_client_connects"
-python3 -m unittest examples.ReferenceTest.test_reference.Test_Reference._runtest_client_connects; ((test_exit_code = test_exit_code || $?))
 
 echo "Terminating SDCri provider"
 jobs && kill %1
