3d353d9b5eaa880a764ae18ed3370bb55cce6a11
Albrecht Kleinfeld <albrecht.kleinfeld@draeger.com>
2024-06-28 14:44:57 +0200

    use _runtest_client_connects to not skip unittest

--- sdc11073_consumer.sh
+++ sdc11073_consumer.sh
@@ -29,7 +29,7 @@ cd sdc11073_git
 echo "Starting sdc11073 consumer reference_client"
 python3 -m examples.ReferenceTest.reference_client; ((test_exit_code = $?))
 echo "Starting sdc11073 consumer test_client_connects"
-python3 -m unittest examples.ReferenceTest.test_reference.Test_Reference.test_client_connects; ((test_exit_code = test_exit_code || $?))
+python3 -m unittest examples.ReferenceTest.test_reference.Test_Reference._runtest_client_connects; ((test_exit_code = test_exit_code || $?))
 
 echo "Terminating SDCri provider"
 jobs && kill %1
