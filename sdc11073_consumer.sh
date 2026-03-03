67a270db79b76c4d5e4f3eae5886a6b2b7a647c1
a-kleinf <87371692+a-kleinf@users.noreply.github.com>
2026-03-03 20:33:39 +0100

    Test sdc11073 v2.3.0 against SDCri 7.0.0

--- sdc11073_consumer.sh
+++ sdc11073_consumer.sh
@@ -27,9 +27,9 @@ sleep 20
 
 cd sdc11073_git 
 echo "Starting sdc11073 consumer reference_consumer"
-python3 -m examples.ReferenceTest.reference_consumer; ((test_exit_code = $?))
+python3 -m pat.ReferenceTest.reference_consumer; ((test_exit_code = $?))
 echo "Starting sdc11073 consumer test_client_connects"
-python3 -m unittest examples.ReferenceTest.test_reference.Test_Reference.test_client_connects; ((test_exit_code = test_exit_code || $?))
+python3 -m unittest pat.ReferenceTest.test_reference.Test_Reference.test_client_connects; ((test_exit_code = test_exit_code || $?))
 
 echo "Terminating SDCri provider"
 jobs && kill %1
