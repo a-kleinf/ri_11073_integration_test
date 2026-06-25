a82d4234fc75cc9eb9405fdbbcd6254172a1cf4c
a-kleinf <87371692+a-kleinf@users.noreply.github.com>
2026-06-25 11:05:21 +0200

    Wait 90 sec for the ri-consumer to initialize before starting the provider.

--- ri_consumer_first.sh
+++ ri_consumer_first.sh
@@ -37,7 +37,7 @@ EOF
 (cd sdpi_git && ./gradlew run -PchooseMain=org.somda.sdpi.test.v2.consumer.MainKt --args="--config ${config}") &
 consumer_pid=$!
 
-sleep 30  # Wait for the consumer to initialize before starting the provider.
+sleep 90  # Wait for the consumer to initialize before starting the provider.
 
 # Then start the sdc11073 provider.
 if [ "${args[0]}" == "true" ]; then
